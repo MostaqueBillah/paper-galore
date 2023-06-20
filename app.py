397989ecbeeca0730c1401fa8fe9e8168d33639f
Mostaque <mostaquebillah@gmail.com>
2023-06-21 02:18:55 +0600

    Initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home") #decorator 
+def home():
+    return render_template('index.html')
+
+@app.route("/about") #decorator 
+def about():
+
+    return "<h1>About Page</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
